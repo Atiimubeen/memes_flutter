71e536059cb26b4fb2d1ab317410bcdbce5b506e
Atiimubeen <atii237237@gmail.com>
2025-06-12 09:40:54 +0500

    Adding the premission of internet

--- lib/services/memes_services.dart
+++ lib/services/memes_services.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'
-    as http; // You need to import http package as htt
+import 'package:http/http.dart' as http;
 import 'package:memes_flutter/models/memes_model.dart';
 
 class MemesServices {
@@ -9,10 +8,9 @@ class MemesServices {
     final url = Uri.parse('https://meme-api.com/gimme/wholesomememes/50');
 
     try {
-      final response = await http.get(url); // Corrected 'respone' to 'response'
+      final response = await http.get(url);
 
       if (response.statusCode == 200) {
-        // Check the statusCode, not the response body
         final data = jsonDecode(response.body);
         final memes = (data['memes'] as List)
             .map((meme) => Meme.fromJson(meme))
@@ -25,7 +23,7 @@ class MemesServices {
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(const SnackBar(content: Text("Error loading memes")));
-      return []; // Return an empty list in case of error
+      return [];
     }
   }
 }
